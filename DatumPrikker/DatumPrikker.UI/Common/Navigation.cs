09bd346b618ca2cc7ff0ff8b7dae523c785e50a1
Tim Jansen <tjajansen@gmail.com>
2013-08-06 21:21:27 +0200

    Skeleton 0.1

---
+++ DatumPrikker/DatumPrikker.UI/Common/Navigation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace DatumPrikker.UI.Common
+{
+    internal class Navigation
+    {
+        public static void GoTo(Type window)
+        {
+            var frame = new Frame();
+            frame.Navigate(window);
+            Window.Current.Content = frame;
+        }
+    }
+}
